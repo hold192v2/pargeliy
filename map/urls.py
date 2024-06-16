12f013c16da769b0a78718e0b613677b64c17056
Hold192 <agrigorev664@gmail.com>
2024-06-17 01:03:21 +0500

    Новая версия ArtEkat  - все карты готовы, начало работы над карточками

--- map/urls.py
+++ map/urls.py
@@ -5,6 +5,14 @@ app_name = 'map'
 
 urlpatterns = [
     path('Ordzmap/', views.map, name='map'),
+    path('Ordzmap/POST/', views.submit_data, name='OrdzmapPOST'),
+    path('Chkalovmap/POST/', views.submit_data, name='ChkalovmapPOST'),
+    path('Kirovmap/POST/', views.submit_data, name='KirovmapPOST'),
+    path('Leninmap/POST/', views.submit_data, name='LeninmapPOST'),
+    path('Oktyabrmap/POST/', views.submit_data, name='OktyabrmapPOST'),
+    path('VerhIsetmap/POST/', views.submit_data, name='VerhIsetmapPOST'),
+    path('Zheleznmap/POST/', views.submit_data, name='ZheleznmapPOST'),
+
     path('Chkalovmap/', views.Chkalovsky, name='chkalovsky'),
     path('Kirovmap/', views.Kirovsky, name='kirovsky'),
     path('Leninmap/', views.Leninsky, name='leninsky'),
--- map/views.py
+++ map/views.py
@@ -1,142 +1,50 @@
+import json
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
 from map.models import Cardss
 
-
+def is_ajax(self):
+    return self.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
 def map(request):
-    selected_objects = [1, 3, 6 ,8 ,12] #тут id
-    coordinates = Cardss.objects.values_list('coordinates', flat=True) #тут список координат
+    return render(request, 'map/map.html' )
 
-    if len(selected_objects) == 1:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 2:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}'
-    elif len(selected_objects) == 3:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 4:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 5:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 6:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}?m=60.581212%2C56.8318%2F13.51'
-    else:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}{coordinates[selected_objects[6]]}?m=60.581212%2C56.8318%2F13.51'
-    return render(request, 'map/map.html', {'routeURL': route_url})
+def submit_data(request):
+    if request.method == 'POST':
+        selected_objects = json.loads(request.body)  # тут slugи
+        slugs = Cardss.objects.values_list('slug', flat=True)
+        coordinates = Cardss.objects.values_list('coordinates', flat=True)  # тут список координат
+
+        if len(selected_objects) == 1:
+            route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[list(slugs).index(selected_objects[0])]}?m=60.581212%2C56.8318%2F13.51'
+        elif len(selected_objects) == 2:
+            route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[list(slugs).index(selected_objects[0])]}{coordinates[list(slugs).index(selected_objects[1])]}'
+        elif len(selected_objects) == 3:
+            route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[list(slugs).index(selected_objects[0])]}{coordinates[list(slugs).index(selected_objects[1])]}{coordinates[list(slugs).index(selected_objects[2])]}?m=60.581212%2C56.8318%2F13.51'
+        elif len(selected_objects) == 4:
+            route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[list(slugs).index(selected_objects[0])]}{coordinates[list(slugs).index(selected_objects[1])]}{coordinates[list(slugs).index(selected_objects[2])]}{coordinates[list(slugs).index(selected_objects[3])]}?m=60.581212%2C56.8318%2F13.51'
+        elif len(selected_objects) == 5:
+            route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[list(slugs).index(selected_objects[0])]}{coordinates[list(slugs).index(selected_objects[1])]}{coordinates[list(slugs).index(selected_objects[2])]}{coordinates[list(slugs).index(selected_objects[3])]}{coordinates[list(slugs).index(selected_objects[4])]}?m=60.581212%2C56.8318%2F13.51'
+        elif len(selected_objects) == 6:
+            route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[list(slugs).index(selected_objects[0])]}{coordinates[list(slugs).index(selected_objects[1])]}{coordinates[list(slugs).index(selected_objects[2])]}{coordinates[list(slugs).index(selected_objects[3])]}{coordinates[list(slugs).index(selected_objects[4])]}{coordinates[list(slugs).index(selected_objects[5])]}?m=60.581212%2C56.8318%2F13.51'
+        else:
+            route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[list(slugs).index(selected_objects[0])]}{coordinates[list(slugs).index(selected_objects[1])]}{coordinates[list(slugs).index(selected_objects[2])]}{coordinates[list(slugs).index(selected_objects[3])]}{coordinates[list(slugs).index(selected_objects[4])]}{coordinates[list(slugs).index(selected_objects[5])]}{coordinates[list(slugs).index(selected_objects[6])]}?m=60.581212%2C56.8318%2F13.51'
+        return JsonResponse({'url': route_url})
+
 def Chkalovsky(request):
-    selected_objects = [1, 3, 6 ,8 ,12] #тут id
-    coordinates = Cardss.objects.values_list('coordinates', flat=True) #тут список координат
-
-    if len(selected_objects) == 1:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 2:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}'
-    elif len(selected_objects) == 3:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 4:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 5:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 6:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}?m=60.581212%2C56.8318%2F13.51'
-    else:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}{coordinates[selected_objects[6]]}?m=60.581212%2C56.8318%2F13.51'
-    return render(request, 'map/Chkalovsky.html', {'routeURL': route_url})
+    return render(request, 'map/Chkalovsky.html')
 def Kirovsky(request):
-    selected_objects = [1, 3, 6 ,8 ,12] #тут id
-    coordinates = Cardss.objects.values_list('coordinates', flat=True) #тут список координат
-
-    if len(selected_objects) == 1:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 2:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}'
-    elif len(selected_objects) == 3:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 4:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 5:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 6:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}?m=60.581212%2C56.8318%2F13.51'
-    else:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}{coordinates[selected_objects[6]]}?m=60.581212%2C56.8318%2F13.51'
-    return render(request, 'map/Kirovsky.html', {'routeURL': route_url})
+    return render(request, 'map/Kirovsky.html')
 def Leninsky(request):
-    selected_objects = [1, 3, 6 ,8 ,12] #тут id
-    coordinates = Cardss.objects.values_list('coordinates', flat=True) #тут список координат
-
-    if len(selected_objects) == 1:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 2:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}'
-    elif len(selected_objects) == 3:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 4:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 5:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 6:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}?m=60.581212%2C56.8318%2F13.51'
-    else:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}{coordinates[selected_objects[6]]}?m=60.581212%2C56.8318%2F13.51'
-    return render(request, 'map/Leninsky.html', {'routeURL': route_url})
+    return render(request, 'map/Leninsky.html')
 def Oktyabrsky(request):
-    selected_objects = [1, 3, 6 ,8 ,12] #тут id
-    coordinates = Cardss.objects.values_list('coordinates', flat=True) #тут список координат
-
-    if len(selected_objects) == 1:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 2:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}'
-    elif len(selected_objects) == 3:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 4:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 5:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 6:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}?m=60.581212%2C56.8318%2F13.51'
-    else:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}{coordinates[selected_objects[6]]}?m=60.581212%2C56.8318%2F13.51'
-    return render(request, 'map/Oktyabrsky.html', {'routeURL': route_url})
+    return render(request, 'map/Oktyabrsky.html')
 def VerhIsetsky(request):
-    selected_objects = [1, 3, 6 ,8 ,12] #тут id
-    coordinates = Cardss.objects.values_list('coordinates', flat=True) #тут список координат
-
-    if len(selected_objects) == 1:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 2:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}'
-    elif len(selected_objects) == 3:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 4:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 5:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 6:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}?m=60.581212%2C56.8318%2F13.51'
-    else:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}{coordinates[selected_objects[6]]}?m=60.581212%2C56.8318%2F13.51'
-    return render(request, 'map/VerhIsetsky.html', {'routeURL': route_url})
+    return render(request, 'map/VerhIsetsky.html')
 def Zhelezn(request):
-    selected_objects = [1, 3, 6 ,8 ,12] #тут id
-    coordinates = Cardss.objects.values_list('coordinates', flat=True) #тут список координат
-
-    if len(selected_objects) == 1:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 2:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}'
-    elif len(selected_objects) == 3:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 4:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 5:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}?m=60.581212%2C56.8318%2F13.51'
-    elif len(selected_objects) == 6:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}?m=60.581212%2C56.8318%2F13.51'
-    else:
-        route_url = f'https://2gis.ru/ekaterinburg/directions/points/{coordinates[selected_objects[0]]}{coordinates[selected_objects[1]]}{coordinates[selected_objects[2]]}{coordinates[selected_objects[3]]}{coordinates[selected_objects[4]]}{coordinates[selected_objects[5]]}{coordinates[selected_objects[6]]}?m=60.581212%2C56.8318%2F13.51'
-    return render(request, 'map/Zhelezn.html', {'routeURL': route_url})
+    return render(request, 'map/Zhelezn.html')
 def handle_rating(request):
     if request.method == 'POST':
         rating = request.POST.get('rating', None)
